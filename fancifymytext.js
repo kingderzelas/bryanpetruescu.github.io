0f33a4e8a4898f696ea8a6be8206026d38aa450b
kingderzelas <56740958+kingderzelas@users.noreply.github.com>
2021-10-09 15:13:32 -0700

    Reverted passing id into getElementById

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,10 +1,10 @@
-function biggerButton(id) {
-	document.getElementById(id).style.fontSize = "24pt";
+function biggerButton() {
+	document.getElementById("text").style.fontSize = "24pt";
 }
 
 function radioSelected(id) {
     if (id === "FancyShmancy") {
-        document.getElementById(id).style.fontWeight = "bold";
+        document.getElementById("FancyShmancy").style.fontWeight = "bold";
     } else if (id === "BoringBetty") {
         alert("BoringBetty");
     } else {
