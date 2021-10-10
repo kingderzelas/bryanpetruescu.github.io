a9e5adec3b7443f97f09c9d60ff49d6ff39aae67
kingderzelas <56740958+kingderzelas@users.noreply.github.com>
2021-10-09 21:22:20 -0700

    Cleaned up index and finished fancifying

--- fancifymytext.js
+++ fancifymytext.js
@@ -6,9 +6,11 @@ function radioSelected(id) {
     if (id === "FancyShmancy") {
         document.getElementById("text").style.fontWeight = "bold";
         document.getElementById("text").style.color = "blue";
+        document.getElementById("text").style.textDecoration = "underline";
     } else if (id === "BoringBetty") {
         document.getElementById("text").style.fontWeight = "normal";
         document.getElementById("text").style.color = "black";
+        document.getElementById("text").style.textDecoration = "none";
     } else {
         alert("Error: Invalid Selection");
     }
