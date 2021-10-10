0892c7f097a508753099ffc8f908863da820663d
kingderzelas <56740958+kingderzelas@users.noreply.github.com>
2021-10-09 21:17:41 -0700

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -5,8 +5,10 @@ function biggerButton() {
 function radioSelected(id) {
     if (id === "FancyShmancy") {
         document.getElementById("text").style.fontWeight = "bold";
+        document.getElementById("text").style.color = "blue";
     } else if (id === "BoringBetty") {
         document.getElementById("text").style.fontWeight = "normal";
+        document.getElementById("text").style.color = "black";
     } else {
         alert("Error: Invalid Selection");
     }
